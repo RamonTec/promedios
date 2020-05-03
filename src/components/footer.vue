bc63fed06ca294c3394ab69cc514807d89fdac37
elias estrabao <eestrabao46@gmail.com>
2020-05-02 22:55:01 -0400

    Componentes agregados

---
+++ src/components/footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-negative row justify-center q-mt-xl">
+    <div class="text-center">
+      <q-card-section class="col-gutter-xs col-xs-12 col-sm-12 col-md-2 q-mt-sm q-mb-sm">
+        <img src="../statics/pro/logo.png" class="" style="height: 60px;"/>
+      </q-card-section>
+    </div>
+    <q-separator style="width:2px; height:35px; margin-top:30px;" class="xs-hide" color="white" vertical inset />
+    <div>
+      <q-card-section class="texto3 text-center text-h6 col-gutter-xs col-xs-12 col-sm-12 col-md-3 q-mb-md" style="margin-top:20px;">
+        2020 Todos los derechos reservados.
+      </q-card-section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/formulario.vue
@@ -0,0 +1,342 @@
+<template>
+  <div>
+    <div class="img_group2 row justify-around">
+      <div class="col-md-7">
+        <div class="xs-hide sm-hide row">
+          <div class="col-md-12 bg-none q-mt-xl">
+            <q-card-section>
+              <q-toolbar-title class="texto1 q-ml-md text-h2" style="white-space: initial; line-height: 50pt;">
+                Listo para entrar en la
+                transformación digital?
+              </q-toolbar-title>
+            </q-card-section>
+          </div>
+        </div>
+        <div class="xs-hide sm-hide row">
+          <div class="col-md-10">
+            <q-card-section class="q-ml-xl">
+              <q-toolbar-title class="texto2 text-h4" style="white-space: initial; line-height: 20pt;">
+                Solo tendras exito si crees que puedes!
+              </q-toolbar-title>
+            </q-card-section>
+          </div>
+          <div class="col-md-10">
+            <q-card-section class="texto3 q-ml-sm">
+              <q-toolbar-title class="q-ml-xl" style="white-space: initial; line-height: 20pt;">
+                Trabajemos juntos para ser más grandes.
+              </q-toolbar-title>
+            </q-card-section>
+            <q-card-section class="col-md-1">
+              <img 
+              style="width: 40%; height: 150%; margin-left:85%; margin-top:-50px;"
+              src="../statics/pro/flecha.svg">
+            </q-card-section>
+          </div>
+          <div class="xs-hide sm-hide">
+            
+          </div>  
+        </div>
+
+        <div class="xs-hide row" style="margin-top:20px;">
+          <div class="col-xs-5 col-sm-6 col-md-6">
+            <q-card-section class="texto1">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/col.svg" style="width: 40px; height: 20px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h3" style="white-space: initial;">Bogotá</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+            <q-card-section style="margin-top:-15px;" class="texto3">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/marcador.svg" style="width: 40px; height: 40px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h6" style="white-space: initial;">Carrera 13 #93-35, Piso 6 Counity Coworking</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+            <q-card-section style="margin-top:-20px;" class="texto3">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/mensaje.svg" style="width: 40px; height: 40px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h6" style="white-space: initial; overflow: hidden;">hola@promediosdigitales
+                    .com</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+            <q-card-section style="margin-top:-20px;" class="texto3">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/llamada.svg" style="width: 40px; height: 40px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h6" style="white-space: initial;">+ 57 315 839 4620</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+            <q-card-section style="margin-top:-20px;" class="texto3">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/whatsapp.svg" style="width: 40px; height: 40px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h6" style="white-space: initial;">Contáctanos vía whatsapp</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+          </div>
+
+          <div class="col-xs-5 col-sm-6 col-md-6">
+            <q-card-section class="texto1">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/cad.svg" style="width: 40px; height: 20px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h3" style="white-space: initial;">Montreal</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+            <q-card-section class="texto3" style="margin-top:-20px;">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/marcador.svg" style="width: 40px; height: 40px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h6" style="white-space: initial;">Carrera 13 #93-35, Piso 6 Counity Coworking</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+            <q-card-section style="margin-top:-20px;" class="texto3">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/mensaje.svg" style="width: 40px; height: 40px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h6" style="overflow: hidden;">hola@promediosdigitales .com</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+            <q-card-section style="margin-top:-20px;" class="texto3">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/llamada.svg" style="width: 40px; height: 40px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h6" style="white-space: initial;">+ 57 315 839 4620</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+            <q-card-section style="margin-top:-20px;" class="texto3">
+              <q-item>
+                <q-item-section side>
+                  <q-avatar>
+                    <q-img src="../statics/pro/whatsapp.svg" style="width: 40px; height: 40px;"></q-img>
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-h6" style="white-space: initial;">Contáctanos vía whatsapp</span>
+                </q-item-section>
+              </q-item>                
+            </q-card-section>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="xs-hide sm-hide col-md-4 q-mt-md q-ml-xl q-mb-xl">
+        <div class="row q-mt-xl"></div>
+        <div class="row q-mt-md"></div>
+        <q-card flat bordered class="bg-white q-mr-md q-mt-xl" style="border-radius:25px">
+              <div class="col-md-12">
+                <q-card-section>
+                  <q-toolbar-title class="texto1 q-ml-sm text-h6 text-start text-black">
+                    Cuéntanos acerca de ti
+                  </q-toolbar-title>
+                  <q-toolbar-title class="text-h6 text-black text-weight-light">
+                    Sobre la empresa ó el proyecto
+                  </q-toolbar-title>
+                </q-card-section>
+              </div>
+            <div class="row justify-center">
+              <div class="col-11">
+                <q-input class="q-mt-sm" outlined v-model="text" label="Nombre de la empresa ó proyecto" style="border-radius:10px" dense />
+                <q-select class="q-mt-sm" outlined v-model="text" label="Sector al que pertenece" style="border-radius:10px" dense />
+                <q-select class="q-mt-sm" outlined v-model="text" label="En que servicio estas interesado?" style="border-radius:10px" dense />
+                <q-select class="q-mt-sm" outlined v-model="text" label="Tipo de negocio ó proyecto" style="border-radius:10px" dense />
+              </div>
+            </div>
+            <div class="col-10">
+              <q-card-section>
+                  <q-toolbar-title class="text-h6 text-black text-weight-light">
+                    Datos de contacto
+                  </q-toolbar-title>
+                </q-card-section>
+            </div>
+            <div class="row justify-center">
+              <div class="col-11">
+                <div class="row justify-center">
+                  <div class="col-6">
+                    <q-input class="q-mt-sm q-mr-sm" outlined v-model="text" label="Nombres" style="border-radius:10px" dense />
+                  </div>
+                  <div class="col-6">
+                    <q-input class="q-mt-sm " outlined v-model="text" label="Apellidos" style="border-radius:10px" dense />
+                  </div>
+                </div>
+                <div class="row justify-center">
+                  <div class="col-6">
+                    <q-input class="q-mt-sm q-mr-sm" outlined v-model="text" label="E - Mail" style="border-radius:10px" dense />
+                  </div>
+                  <div class="col-6">
+                    <q-input class="q-mt-sm " outlined v-model="text" label="Celular" style="border-radius:10px" dense />
+                  </div>
+                </div>
+                <div class="row">
+                  <q-input
+                    class="q-mt-sm col-12"
+                    v-model="text"
+                    outlined
+                    label="Cuéntanos que expectativa tienes"
+                    type="textarea"
+                  />
+                </div>
+                <div class="row">
+                  <div class="col-4 q-my-md">
+                    <q-btn color="positive" label="Enviar" size="lg" class="full-width"></q-btn>
+                  </div>
+                  <div class="col-7 offset-1 q-my-md">
+                    <span style="color:#000">Los datos ingresados serán tratados de manera confidencial como lo dispone la ley habeas data.</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </q-card>
+      </div>
+      
+    </div>
+
+
+
+
+    <div class="md-hide lg-hide xl-hide row justify-center q-mt-md q-mb-md">
+      <div class="texto1 col-xs-10 col-sm-10 text-h4">
+        ¿Listo para entrar en la
+        transformación digital?
+      </div>
+      <div class="texto3 col-xs-10 col-sm-10 text-h6">
+        Solo tendras exito si crees que puedes!
+        Trabajemos juntos para ser más grandes.
+      </div>
+    </div>
+
+    <div class="md-hide lg-hide xl-hide">
+      <div class="md-hide lx-hide lg-hide row justify-center">
+        <div class="col-xs-10 col-sm-10">
+          <q-card flat bordered class="bg-white" style="border-radius:25px">
+            <div class="col-xs-10">
+
+              <q-card-section>
+                <q-toolbar-title class="texto1 q-ml-sm text-h6 text-start text-black">
+                  Cuéntanos acerca de ti
+                </q-toolbar-title>
+                <q-toolbar-title class="text-h6 text-black text-weight-light">
+                  Sobre la empresa ó el proyecto
+                </q-toolbar-title>
+              </q-card-section>
+            </div>
+
+            <div class="row justify-center">
+              <div class="col-11">
+                <q-input class="q-mt-sm" outlined v-model="text" label="Nombre de la empresa ó proyecto" style="border-radius:10px" dense />
+                <q-select class="q-mt-sm" outlined v-model="text" label="Sector al que pertenece" style="border-radius:10px" dense />
+                <q-select class="q-mt-sm" outlined v-model="text" label="En que servicio esta interesado?" style="border-radius:10px" dense />
+                <q-select class="q-mt-sm" outlined v-model="text" label="Tipo de negocio ó proyecto" style="border-radius:10px" dense />
+              </div>
+            </div>
+
+            <div class="col-10">
+              <q-card-section>
+                  <q-toolbar-title class="text-h6 text-black text-weight-light">
+                    Datos de contacto
+                  </q-toolbar-title>
+                </q-card-section>
+            </div>
+
+            <div class="row justify-center">
+
+              <div class="col-11">
+                <div class="row justify-center">
+                  <div class="col-6">
+                    <q-input class="q-mt-sm q-mr-sm" outlined v-model="text" label="Nombres" style="border-radius:10px" dense />
+                  </div>
+                  <div class="col-6">
+                    <q-input class="q-mt-sm " outlined v-model="text" label="Apellidos" style="border-radius:10px" dense />
+                  </div>
+                </div>
+                <div class="row justify-center">
+                  <div class="col-6">
+                    <q-input class="q-mt-sm q-mr-sm" outlined v-model="text" label="E - Mail" style="border-radius:10px" dense />
+                  </div>
+                  <div class="col-6">
+                    <q-input class="q-mt-sm " outlined v-model="text" label="Celular" style="border-radius:10px" dense />
+                  </div>
+                </div>
+                <div class="row">
+                  <q-input
+                    class="q-mt-sm col-12"
+                    v-model="text"
+                    outlined
+                    label="Cuéntanos que expectativa tienes"
+                    type="textarea"
+                  />
+                </div>
+                <div class="row">
+                  <div class="col-4 q-my-md">
+                    <q-btn color="positive" label="Enviar" size="lg" class="full-width"></q-btn>
+                  </div>
+                  <div class="col-7 offset-1  q-my-md">
+                    <span style="color:#000">Los datos ingresados serán tratados de manera confidencial como lo dispone la ley habeas data.</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </q-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Formulario'
+}
+</script>
+
+<style>
+
+</style>
