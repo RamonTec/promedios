5113905f5a89ed48c1b1d2937e81578b15967852
Elias Estrabao <eestrabao46@gmail.com>
2020-06-03 13:57:24 -0400

    Prueba

--- quasar.conf.js
+++ quasar.conf.js
@@ -106,7 +106,9 @@ module.exports = function (/* ctx */) {
       ],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Meta'
+      ]
     },
 
     // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
